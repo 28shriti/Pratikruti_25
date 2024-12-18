eb55033b7a504c9881bfddda11cbbc55b22b15a8
rushikesh munne <rushikeshmunne10@gmail.com>
2024-12-18 22:38:37 +0530

    intial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <div id='contact' className="container mx-auto my-10 px-4">
+    <h2 className="text-3xl font-bold mb-4">Contact Us</h2>
+    <p>Email: <span className="text-blue-500">contact@event.com</span></p>
+    <p>Phone: <span className="text-blue-500">+1-234-567-890</span></p>
+  </div>
+);
+
+export default Contact;
